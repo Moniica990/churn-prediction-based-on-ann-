779347be884c47563f19287108bf641487baf9b9
Moniica <157867338+Moniica990@users.noreply.github.com>
2025-08-25 11:39:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import tensorflow as tf
-from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
 import pickle
 
@@ -19,25 +18,25 @@ with open('scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
 
-## streamlit app
-st.title('Customer Churn PRediction')
+# Streamlit app
+st.title('Customer Churn Prediction')
 
-# User input
-geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
-gender = st.selectbox('Gender', label_encoder_gender.classes_)
+# User inputs
+geography = st.selectbox('Geography', options=onehot_encoder_geo.categories_[0])
+gender = st.selectbox('Gender', options=label_encoder_gender.classes_)
 age = st.slider('Age', 18, 92)
-balance = st.number_input('Balance')
-credit_score = st.number_input('Credit Score')
-estimated_salary = st.number_input('Estimated Salary')
+balance = st.number_input('Balance', min_value=0.0, format="%.2f")
+credit_score = st.number_input('Credit Score', min_value=0, max_value=1000, step=1)
+estimated_salary = st.number_input('Estimated Salary', min_value=0.0, format="%.2f")
 tenure = st.slider('Tenure', 0, 10)
 num_of_products = st.slider('Number of Products', 1, 4)
-has_cr_card = st.selectbox('Has Credit Card', [0, 1])
-is_active_member = st.selectbox('Is Active Member', [0, 1])
+has_cr_card = st.selectbox('Has Credit Card', options=[0, 1])
+is_active_member = st.selectbox('Is Active Member', options=[0, 1])
 
-# Prepare the input data
+# Prepare input dataframe
 input_data = pd.DataFrame({
     'CreditScore': [credit_score],
-    'Gender': [label_encoder_gender.transform([gender])[0]],
+    'Gender': label_encoder_gender.transform([gender]),  # Encode gender here
     'Age': [age],
     'Tenure': [tenure],
     'Balance': [balance],
@@ -47,24 +46,23 @@ input_data = pd.DataFrame({
     'EstimatedSalary': [estimated_salary]
 })
 
-# One-hot encode 'Geography'
+# One-hot encode geography
 geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
 geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
 
-# Combine one-hot encoded columns with input data
+# Concatenate
 input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
 
-# Scale the input data
+# Scale the data
 input_data_scaled = scaler.transform(input_data)
 
-
-# Predict churn
+# Prediction
 prediction = model.predict(input_data_scaled)
 prediction_proba = prediction[0][0]
 
 st.write(f'Churn Probability: {prediction_proba:.2f}')
 
 if prediction_proba > 0.5:
-    st.write('The customer is likely to churn.')
+    st.success('The customer is likely to churn.')
 else:
-    st.write('The customer is not likely to churn.')
+    st.info('The customer is not likely to churn.')
